f41a1f1af524fa7e8cac5c5018c8fa56f3b8d702
qishilong <663253062@qq.com>
2023-02-11 18:28:22 +0800

    其他常用类型

--- Typescript 基础/03.基本类型检查/src/index.ts
+++ Typescript 基础/03.基本类型检查/src/index.ts
@@ -29,3 +29,55 @@ const arr: number[] = [];
 const bool: boolean = true;
 const nu: null = null;
 const undef: undefined = undefined;
+
+// 联合类型
+let maybe: string | undefined;
+
+// 类型保护
+let two: boolean | number = 1;
+function judge(param: boolean | number) {
+    if (typeof param === "boolean") {
+        param = true;
+    }
+}
+
+judge(two);
+
+// void 类型
+function fun1(): void {
+
+}
+
+// never 类型
+function fun2(msg: string): never {
+    throw new Error(msg);
+}
+
+// 字面量类型
+let user: {
+    name: string,
+    age: number;
+};
+user = {
+    name: "小明",
+    age: 18
+};
+
+let gender: "男" | "女";
+gender = "男";
+
+let arr1: [];
+
+// 元组类型
+let arr2: [string, number];
+arr2 = ["1", 1];
+
+// any 类型
+let obj1: any;
+obj1 = 1;
+
+let obj2: object;
+obj2 = {
+    a: "1",
+    b: 1
+};
