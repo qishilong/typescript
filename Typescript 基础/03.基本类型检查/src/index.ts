75717c55e1fb9561895da28983292eb620de430c
qishilong <663253062@qq.com>
2023-02-11 17:00:05 +0800

    类型推导和编译结果对比

---
+++ Typescript 基础/03.基本类型检查/src/index.ts
@@ -0,0 +1,16 @@
+// function sum(a: number, b: number): number {
+//     return a + b;
+// }
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+// sum("1", "2"); 
+console.log(sum(1, 2));
+
+let phone: number = 1234545544;
+/**
+ * 虽然没有明确规定，但根据习惯，电话号码应该是字符串格式
+ */
+let phone1: string = "123456654";
