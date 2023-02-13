8f238a7334a47bb13041c55ef8e40382b2c7895e
qishilong <663253062@qq.com>
2023-02-13 12:44:47 +0800

    练习-使用枚举改造打印一副扑克牌

--- Typescript 基础/04.扩展类型-枚举/src/practice/index.ts
+++ Typescript 基础/04.扩展类型-枚举/src/practice/index.ts
@@ -1,17 +1,70 @@
-type Color = "♠️" | "♥️" | "♣️" | "♦️";
+enum Color {
+    hearts = "♥️",
+    diamond = "♦️",
+    spade = "♠️",
+    winterSweet = "♣️"
+}
+
+enum Figure {
+    one = "A",
+    two = "2",
+    three = "3",
+    four = "4",
+    five = "5",
+    six = "6",
+    seven = "7",
+    eight = "8",
+    nine = "9",
+    ten = "10",
+    eleven = "J",
+    twelve = "Q",
+    thirteen = "K",
+
+}
+
 type NormalCard = {
     color: Color;
-    figure: number;
+    figure: Figure;
 };
 type Card = NormalCard[];
 
+
+/**
+ * the first method
+ * 
+const createCard = (): string[] => {
+    let totalCard: string[] = [];
+    const color = Object.values(Color);
+    const figure = Object.values(Figure);
+    // 也可以通过使用Object.keys()获取Color,Figure的对应的属性名，然后根据属性值得到属性值，不过稍微麻烦了点
+    // const color = Object.keys(Color);
+    // const figure = Object.keys(Figure);
+    for (const colorValue of color) {
+        for (const figureValue of figure) {
+            totalCard.push(`${colorValue}${figureValue} `);
+            // totalCard.push(`${Color[colorValue]}${Figure[figureValue]} `);
+        }
+    }
+    return totalCard;
+};
+const cards = createCard();
+console.log(cards);
+*/
+
+/**
+ * the second method
+ *
 const createCard = (): Card => {
     let totalCard: Card = [];
-    for (let i = 1; i <= 13; i++) {
-        totalCard.push({ color: "♠️", figure: i });
-        totalCard.push({ color: "♣️", figure: i });
-        totalCard.push({ color: "♥️", figure: i });
-        totalCard.push({ color: "♦️", figure: i });
+    const color = Object.values(Color);
+    const figure = Object.values(Figure);
+    for (const colorValue of color) {
+        for (const figureValue of figure) {
+            totalCard.push({
+                color: colorValue,
+                figure: figureValue
+            });
+        }
     }
     return totalCard;
 };
@@ -19,25 +72,16 @@ const createCard = (): Card => {
 const printCard = (card: Card) => {
     let result = "";
     card.forEach((item, i) => {
-        let cardColor = item.color;
-        if (item.figure === 1) {
-            cardColor += "A";
-        } else if (item.figure >= 2 && item.figure <= 10) {
-            cardColor += item.figure;
-        } else if (item.figure === 11) {
-            cardColor += "J";
-        } else if (item.figure === 12) {
-            cardColor += "Q";
-        } else {
-            cardColor += "K";
-        }
+        let cardColor = item.color + item.figure;
         result += cardColor + "\t";
         if ((i + 1) % 4 === 0) {
             result += "\n";
         }
     });
-    console.log(result);
+    return result;
 };
 
 const cards = createCard();
-printCard(cards);
+console.log(printCard(cards))
+ */
+
