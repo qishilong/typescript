b587751e12dd511532af504bba391063149a5bb1
qishilong <663253062@qq.com>
2023-02-14 23:17:42 +0800

    概述

---
+++ Typescript 基础/07.ts 中的类/learn/src/01.概述/index.ts
@@ -0,0 +1,10 @@
+class user {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    };
+}
+
+const userClass = new user("a", 18);
